b45324b9cb274b0d3ff0e2a68e4668e8789c5130
Dipan Sarkar <alexmithun1005@gmial.com>
2024-11-16 12:10:49 +0530

    navbar fixed

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,17 +1,16 @@
-import Search from '../Search/Search'
-import Banner from '../../components/Banner/Banner'
-import Accordian from '../../components/Accordian/Accordian'
-
+import Search from "../Search/Search";
+import Banner from "../../components/Banner/Banner";
+import Accordian from "../../components/Accordian/Accordian";
+import "./home.css";
 
 const Home = () => {
   return (
     <>
-   
-    <Search/>
-    <Banner/>
-    <Accordian/>
+      <Search />
+      <Banner />
+      <Accordian />
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,19 +1,63 @@
-import { Link , NavLink } from 'react-router-dom'
+import { Link, NavLink } from "react-router-dom";
+import "./navbar.css";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { RxCross1 } from "react-icons/rx";
+
+import { useState } from "react";
 
 const Navbar = () => {
- 
-    const navItems = [
-        {path : "/" , element :"home" } , {path : "/exchanges" , element :"exchanges" } , {path : "/coins" , element :"coins" }  , {path : "/more" , element :"more" }  , ]
-
+  const navItems = [
+    { path: "/", element: "home" },
+    { path: "/exchanges", element: "exchanges" },
+    { path: "/coins", element: "coins" },
+    { path: "/more", element: "more" },
+  ];
+  const [menuOpen, setMenuOpen] = useState(false);
+  console.log(menuOpen);
   return (
-<nav className='navbar'>
-    <Link to={"/"} className='logo'>CoinX</Link>
-    <ul className='navList'>
-        {navItems.map((item , index)=> { 
-        return ( <NavLink className='listItem' to={item.path} key={index}>{item.element} </NavLink> 
-)})}
-    </ul>
-</nav>  )
-}
+    <>
+      <nav className="navbar">
+        <Link to={"/"} className="logo">
+          CoinX
+        </Link>
+        <ul className="navList">
+          {navItems.map((item, index) => {
+            return (
+              <NavLink className="listItem" to={item.path} key={index}>
+                {item.element}{" "}
+              </NavLink>
+            );
+          })}
+        </ul>
+        <div
+          className="hamburgerMenu"
+          type="button"
+          onClick={() => setMenuOpen(!menuOpen)}
+        >
+          {menuOpen ? <RxCross1 /> : <RxHamburgerMenu />}
+        </div>
+      </nav>
 
-export default Navbar
+      {menuOpen && (
+        <div className="mobileMenuOverlay">
+          <div className="mobileMenu">
+            {navItems.map((item, index) => {
+              return (
+                <Link
+                  to={item.path}
+                  onClick={() => setMenuOpen(false)}
+                  className="mobileLink"
+                  key={index}
+                >
+                  {item.element}
+                </Link>
+              );
+            })}
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Navbar;
